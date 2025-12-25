ba3107947f86360e1fc605ca3aa18546835332f8
Ersan Karimi <ersankarimii311@gmail.com>
2025-12-26 01:27:04 +0800

    feat: enhance ESLint configuration with antfu presets and additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,37 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt({
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
-})
+export default withNuxt(
+  antfu({
+    vue: true,
+    jsonc: true,
+    markdown: true,
+    stylistic: {
+      indent: 2,
+      quotes: "double",
+      semi: true,
+    },
+    rules: {
+      "no-console": "warn",
+    },
+    formatters: {
+      markdown: "prettier",
+      css: "prettier",
+      prettierOptions: {
+        singleQuote: false,
+        semi: true,
+        tabWidth: 2,
+        useTabs: false,
+      },
+    },
+    ignores: [
+      "dist",
+      "node_modules",
+      ".nuxt",
+      "pnpm-lock.yaml",
+      ".github/workflows",
+      "pnpm-workspace.yaml",
+    ],
+  }),
+);
